31b7f929eec2ad7961bb2a5715877fdff7edf1ca
Ariadna2809 <1219100450@alumnos.utng.edu.mx>
2022-08-05 12:50:40 -0500

    Modificacion

--- proj1/settings.py
+++ proj1/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +25,7 @@ SECRET_KEY = 'django-insecure-5sk#w^tp863)kwwf%jh%g(l=pwlxxbc33*yijhgs4!x80o=i22
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'dloredo.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'dloredo.herokuapp.com']
 
 
 # Application definition
@@ -118,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = (BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = (BASE_DIR, "static")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
